41fc1ff650a5c42e9da125d5b414106229c81c0d
acigankov@inbox.ru <diiPs56uaY>
2025-10-17 17:21:26 +0300

    edit template

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -333,13 +333,38 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
                             <p>Дашборд</p>
                         </a>
                     </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link ">
+                            <i class="nav-icon bi bi-card-list"></i>
+                            <p>Заявки</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link ">
+                            <i class="nav-icon bi bi-card-text"></i>
+                            <p>Задачи</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link ">
+                            <i class="nav-icon bi bi-card-text"></i>
+                            <p>Проблемы</p>
+                        </a>
+                    </li>
 
-                    <li class="nav-header">Настройки</li>
+                    <li class="nav-header">Администрирование</li>
 
                     <li class="nav-item">
                         <a href="/" class="nav-link ">
-                            <i class="nav-icon bi bi-tools"></i>
+                            <i class="nav-icon bi bi-gear"></i>
                             <p>Настройки</p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="/" class="nav-link ">
+                            <i class="nav-icon bi bi-people-fill"></i>
+                            <p>Пользователи</p>
                         </a>
                     </li>
 
